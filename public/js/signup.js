217a1ac28d5ded0f2160af9931b2d9d1dc8815f2
Silver Snail 57 <cdarkly1392@gmail.com>
2020-05-21 02:11:39 -0400

    Styled user and index handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  var emailInput = $("input#SUemail-input");
+  var passwordInput = $("input#SUpassword-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
@@ -29,7 +29,7 @@ $(document).ready(function() {
       password: password
     })
       .then(function(data) {
-        window.location.replace("/members");
+        window.location.render("user");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
